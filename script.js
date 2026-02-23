ec051b97d95c719ddf0c3effa9e5adc6531c332c
Service.in <ggiridhar5519@gmail.com>
2026-02-23 11:26:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-/* MOBILE MENU */
 function toggleMenu(){
 document.getElementById("nav").classList.toggle("active");
 }
 
-/* FILTER */
+/* FILTER WITH FADE */
 function filterProjects(category){
 let items=document.querySelectorAll(".portfolio-item");
 let buttons=document.querySelectorAll(".filter-buttons button");
@@ -12,11 +11,15 @@ buttons.forEach(btn=>btn.classList.remove("active"));
 event.target.classList.add("active");
 
 items.forEach(item=>{
+item.style.opacity="0";
+setTimeout(()=>{
 if(category==="all"||item.classList.contains(category)){
 item.style.display="block";
+item.style.opacity="1";
 }else{
 item.style.display="none";
 }
+},300);
 });
 }
 
