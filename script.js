c3041270b11d4cf3ca0ebe9cf545825a46b72705
Service.in <ggiridhar5519@gmail.com>
2026-02-23 11:37:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-/* MOBILE MENU */
+let slides=document.querySelectorAll(".slide");
+let current=0;
+
+function nextSlide(){
+slides[current].classList.remove("active");
+current=(current+1)%slides.length;
+slides[current].classList.add("active");
+}
+
+setInterval(nextSlide,3000);
+
 function toggleMenu(){
 document.getElementById("nav").classList.toggle("active");
-}
-
-/* FILTER */
-function filterProjects(category){
-let items=document.querySelectorAll(".portfolio-item");
-let buttons=document.querySelectorAll(".filter-buttons button");
-
-buttons.forEach(btn=>btn.classList.remove("active"));
-event.target.classList.add("active");
-
-items.forEach(item=>{
-if(category==="all"||item.classList.contains(category)){
-item.style.display="block";
-}else{
-item.style.display="none";
-}
-});
-}
-
-/* POPUP */
-function openPopup(element){
-let img=element.querySelector("img").src;
-document.getElementById("popup-img").src=img;
-document.getElementById("popup").style.display="flex";
-}
-function closePopup(){
-document.getElementById("popup").style.display="none";
 }
