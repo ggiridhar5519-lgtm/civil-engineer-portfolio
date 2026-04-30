2188861a24d076aecf8b5f4a036b68eadb900cef
Service.in <ggiridhar5519@gmail.com>
2026-04-30 10:43:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ if (slides.length) {
     slides[current].classList.remove("active");
     current = (current + 1) % slides.length;
     slides[current].classList.add("active");
-  }, 4500);
+  }, 7000);
 }
 
 /* MENU */
